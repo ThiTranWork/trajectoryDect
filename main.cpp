2d820f3c16fa1a8ee89ec436e407f639154b8400
Thi TRAN <thi.tran@uhasselt.be>
2022-08-02 16:33:48 +0200

    correct parameter adjustment and inputs

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@ using namespace std;
 int main () {
 	// TrajData reads in data and holds trajectory information
 	TrajData data;
-	data.readFile("hurricane2000_2006.tra");
+	// data.readFile("hurricane2000_2006.tra"); // test original data
+
+	// data.readFile("data_DJI0111_VRU_Downsample_NoID.tra");
+	data.readFile("data_DJI0111_VRU_Downsample_NoID.tra");
 
 	// COutlierDetector will find outliers in the provided dataset
 	COutlierDetector outlierDetector(&data);
@@ -19,7 +22,7 @@ int main () {
 	cout << "Finished partitioning and finding outliers!\n";
 
 	// Output plot of trajectories and outliers
-	data.OutputTrajectoryPlot("sampleOut.eps");
+	data.OutputTrajectoryPlot("sampleOut_DJI0111_VRU_Downsample.eps");
 
     return 0;
 }
